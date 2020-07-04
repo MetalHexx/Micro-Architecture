075f79421dcb7f2f8b4c4ddada20ed59567f5e2b
William Pereira <MetalHexx@outlook.com>
2020-07-04 19:13:55 -0400

    fetch features from the backend. Should move this to bootstrap later

--- src/Web/src/app/app.component.ts
+++ src/Web/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { FeatureState } from './feature-settings/store/feature.state';
+import { getFeatures } from './feature-settings/store/feature-actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularWeb';
+export class AppComponent implements OnInit {
+
+  constructor(private featureStore: Store<FeatureState>) {
+    
+
+  }
+  ngOnInit(): void {
+    this.featureStore.dispatch(getFeatures());  
+    
+  }
+  
 }

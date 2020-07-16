bd7495ddf1fbc19528f8a59ce5fb108515a73a16
William Pereira <MetalHexx@outlook.com>
2020-07-15 23:51:25 -0400

    updated api clients with new features endpoints

--- src/Web/src/app/gateway-api/models/app-features.ts
+++ src/Web/src/app/gateway-api/models/app-features.ts
@@ -4,6 +4,4 @@ export interface AppFeatures {
   id?: string;
   customerOrdersView?: Feature;
   customerOrderDetails?: Feature;
-  viewCustomers?: Feature;
-  viewOrderDetails?: Feature;
 }
--- src/Web/src/app/gateway-api/services/features.service.ts
+++ src/Web/src/app/gateway-api/services/features.service.ts
@@ -13,6 +13,9 @@ import { AppFeatures } from '../models/app-features';
 })
 class FeaturesService extends __BaseService {
   static readonly GetFeaturesPath = '/api/Features/features';
+  static readonly UpdateFeaturesPath = '/api/Features/features';
+  static readonly CreateFeaturesPath = '/api/Features/features';
+  static readonly DeleteFeaturesPath = '/api/Features/features/{id}';
 
   constructor(
     config: __Configuration,
@@ -53,9 +56,156 @@ class FeaturesService extends __BaseService {
       __map(_r => _r.body as AppFeatures)
     );
   }
+
+  /**
+   * @param params The `FeaturesService.UpdateFeaturesParams` containing the following parameters:
+   *
+   * - `features`:
+   *
+   * @return Success
+   */
+  UpdateFeaturesResponse(params: FeaturesService.UpdateFeaturesParams): __Observable<__StrictHttpResponse<AppFeatures>> {
+    let __params = this.newParams();
+    let __headers = new HttpHeaders();
+    let __body: any = null;
+    __body = params.features;
+    let req = new HttpRequest<any>(
+      'PUT',
+      this.rootUrl + `/api/Features/features`,
+      __body,
+      {
+        headers: __headers,
+        params: __params,
+        responseType: 'json'
+      });
+
+    return this.http.request<any>(req).pipe(
+      __filter(_r => _r instanceof HttpResponse),
+      __map((_r) => {
+        return _r as __StrictHttpResponse<AppFeatures>;
+      })
+    );
+  }
+  /**
+   * @param params The `FeaturesService.UpdateFeaturesParams` containing the following parameters:
+   *
+   * - `features`:
+   *
+   * @return Success
+   */
+  UpdateFeatures(params: FeaturesService.UpdateFeaturesParams): __Observable<AppFeatures> {
+    return this.UpdateFeaturesResponse(params).pipe(
+      __map(_r => _r.body as AppFeatures)
+    );
+  }
+
+  /**
+   * @param params The `FeaturesService.CreateFeaturesParams` containing the following parameters:
+   *
+   * - `features`:
+   *
+   * @return Success
+   */
+  CreateFeaturesResponse(params: FeaturesService.CreateFeaturesParams): __Observable<__StrictHttpResponse<AppFeatures>> {
+    let __params = this.newParams();
+    let __headers = new HttpHeaders();
+    let __body: any = null;
+    __body = params.features;
+    let req = new HttpRequest<any>(
+      'POST',
+      this.rootUrl + `/api/Features/features`,
+      __body,
+      {
+        headers: __headers,
+        params: __params,
+        responseType: 'json'
+      });
+
+    return this.http.request<any>(req).pipe(
+      __filter(_r => _r instanceof HttpResponse),
+      __map((_r) => {
+        return _r as __StrictHttpResponse<AppFeatures>;
+      })
+    );
+  }
+  /**
+   * @param params The `FeaturesService.CreateFeaturesParams` containing the following parameters:
+   *
+   * - `features`:
+   *
+   * @return Success
+   */
+  CreateFeatures(params: FeaturesService.CreateFeaturesParams): __Observable<AppFeatures> {
+    return this.CreateFeaturesResponse(params).pipe(
+      __map(_r => _r.body as AppFeatures)
+    );
+  }
+
+  /**
+   * @param params The `FeaturesService.DeleteFeaturesParams` containing the following parameters:
+   *
+   * - `id`:
+   *
+   * @return Success
+   */
+  DeleteFeaturesResponse(params: FeaturesService.DeleteFeaturesParams): __Observable<__StrictHttpResponse<AppFeatures>> {
+    let __params = this.newParams();
+    let __headers = new HttpHeaders();
+    let __body: any = null;
+
+    let req = new HttpRequest<any>(
+      'DELETE',
+      this.rootUrl + `/api/Features/features/${params.id}`,
+      __body,
+      {
+        headers: __headers,
+        params: __params,
+        responseType: 'json'
+      });
+
+    return this.http.request<any>(req).pipe(
+      __filter(_r => _r instanceof HttpResponse),
+      __map((_r) => {
+        return _r as __StrictHttpResponse<AppFeatures>;
+      })
+    );
+  }
+  /**
+   * @param params The `FeaturesService.DeleteFeaturesParams` containing the following parameters:
+   *
+   * - `id`:
+   *
+   * @return Success
+   */
+  DeleteFeatures(params: FeaturesService.DeleteFeaturesParams): __Observable<AppFeatures> {
+    return this.DeleteFeaturesResponse(params).pipe(
+      __map(_r => _r.body as AppFeatures)
+    );
+  }
 }
 
 module FeaturesService {
+
+  /**
+   * Parameters for UpdateFeatures
+   */
+  export interface UpdateFeaturesParams {
+    features?: AppFeatures;
+  }
+
+  /**
+   * Parameters for CreateFeatures
+   */
+  export interface CreateFeaturesParams {
+    features?: AppFeatures;
+  }
+
+  /**
+   * Parameters for DeleteFeatures
+   */
+  export interface DeleteFeaturesParams {
+    id: string;
+  }
 }
 
 export { FeaturesService }
